61ec907e38dfd9ae747b00add8a57603dea60fe8
CarlVictorV <19104214@usc.edu.ph>
2023-11-26 04:30:53 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,52 +13,62 @@ else:
 def clear(): return os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def menu():
+def display_menu():
     """
     Displays the Menu
-    :return: None
     """
     print('+----------------------------------+')
     print('|--- *IO Management Simulation* ---|')
     print('+----------------------------------+\n')
-    print('[1] FCFS')
-    print('[2] SSTF')
-    print('[3] SCAN')
-    print('[4] CSCAN')
-    print('[5] LOOK')
-    print('[6] CLOOK')
+    print('[1] FCFS - First-Come, First-Served')
+    print('[2] SSTF - Shortest Seek Time First')
+    print('[3] SCAN - Elevator Scan')
+    print('[4] CSCAN - Circular Elevator SCAN')
+    print('[5] LOOK - Elevator Modified LOOK')
+    print('[6] CLOOK - Circular Elevator Modified LOOK\n')
     print('[0] Exit\n')
+
 
 
 def main():
     """
     Main Function
-    :return: None
     """
     while True:
-        menu()
-        choice = int(input('Enter your choice: '))
-        if choice == 0:
-            print('Exiting...')
-            break
-        elif 1 <= choice <= 6:
+        try:
             clear()
-            algorithm_names = {
-                1: 'FCFS', 2: 'SSTF', 3: 'SCAN',
-                4: 'CSCAN', 5: 'LOOK', 6: 'CLOOK'
-            }
-            algorithm_name = algorithm_names[choice] + ' Algorithm'
-            print(algorithm_name)
-            print('------------------------------------')
-            io_algorithm.set_algorithm(algorithm_names[choice])
-            io_algorithm.simulate()
+            display_menu()
+            choice = input('Enter your choice: ')
 
-            # Pause to display results before showing menu again
+            if choice == '0':
+                print('Exiting...')
+                break
+
+            choice = int(choice)
+            if 1 <= choice <= 6:
+                algorithm_names = {
+                    1: 'FCFS', 2: 'SSTF', 3: 'SCAN',
+                    4: 'CSCAN', 5: 'LOOK', 6: 'CLOOK'
+                }
+                algorithm_name = algorithm_names[choice] + ' Algorithm'
+                clear()
+                print(algorithm_name)
+                print('------------------------------------')
+                io_algorithm.set_algorithm(algorithm_names[choice])
+                io_algorithm.simulate()
+                input("Press Enter to continue...")
+            else:
+                clear()
+                print('Invalid Choice! Please choose a number between 0 and 6.')
+                input("Press Enter to continue...")
+        except ValueError:
+            clear()
+            print('Invalid input! Please enter a valid number.')
             input("Press Enter to continue...")
+        except Exception as e:
             clear()
-        else:
-            clear()
-            print('Invalid Choice!')
+            print(f'An error occurred: {str(e)}')
+            input("Press Enter to continue...")
 
 
 if __name__ == '__main__':
